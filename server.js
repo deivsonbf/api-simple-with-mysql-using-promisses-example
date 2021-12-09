3ed9fb36153798c8fc715bddc299bd6156b8910f
Deivsonbf <deivsonb7@gmail.com>
2021-12-09 15:18:26 -0300

    server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const app = require('./index');
 const port = process.env.PORT || 5000
 
 app.listen(port, (req, res) => {
-    if (process.env.NODE_ENV == 'production') {
-        console.log(`${process.env.SERVER}`);
-    } else {
+    if (process.env.NODE_ENV == 'development') {
         console.log(`http://localhost:${port}`);
+    } else {
+        console.log(`${process.env.NODE_ENV}`);
     }
 })
